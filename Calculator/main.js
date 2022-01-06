597c0ebde93b233ea714a5b8df62e0e89f5cc7b9
lcscosta <lucasdacosta@usp.br>
2022-01-06 13:22:25 -0300

    Added sum and minus
    - Added sum and minus when equal operator is clicked.
    - Error in multiply and Divide.

--- Calculator/main.js
+++ Calculator/main.js
@@ -6,7 +6,6 @@ var input = document.getElementById('screen'),
     resultDisplayed = false;
 
 // Adicionar a ação após o click em cada um dos botões
-
 for(i=0; i < number.length; i++){
     number[i].addEventListener("click", function(e){
         var string = input.innerHTML;
@@ -19,3 +18,45 @@ for(i=0; i < number.length; i++){
         }
     })
 }
+
+// Adicionar a ação após o click em cada um dos operadores
+for(i=0; i < operator.length; i++){
+    operator[i].addEventListener("click", function(e){
+        var string = input.innerHTML;
+        var last = string[string.length -1 ];
+
+        input.innerHTML += e.target.innerHTML;
+    })
+}
+
+equal.addEventListener("click", function(e){
+    if (input.innerText.indexOf('+') != "-1"){
+        var numbers = input.innerText.split("+");
+        var sum = 0;
+        for(i = 0; i < numbers.length; i++ ){
+            sum += Number(numbers[i]);
+        }
+        input.innerHTML = sum.toString();
+    } 
+    if (input.innerText.indexOf('-') != "-1") {
+        var numbers = input.innerText.split("-");
+        var minus;
+        minus = Number(numbers[0]) - Number(numbers[1]);
+    
+        input.innerHTML = minus.toString();
+    } 
+    if (input.innerText.indexOf('×') != "-1") {
+        var numbers = input.innerText.split('×');
+        var multi;
+        multi = Number(numbers[0]) * Number(numbers[1]);
+    
+        input.innerHTML = multi.toString();
+    } 
+    if (input.innerText.indexOf('&divide;') != "-1") {
+        var numbers = input.innerText.split("&divide;");
+        var minus;
+        minus = Number(numbers[0]) - Number(numbers[1]);
+    
+        input.innerHTML = minus.toString();
+    } 
+})
